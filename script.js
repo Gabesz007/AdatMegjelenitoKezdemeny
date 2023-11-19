64aa36ef26d7340b40b7dcf5adb264fbbfbd4e84
Gabesz007 <szolgalati.torocsik@gmail.com>
2023-11-19 20:23:42 +0100

    Fejléc hozzáadva.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 console.log("helló")
 // CRUD 
 const adatok =[
-    {id:1,nev:"Micike", faj:"macska", ar:"1000", szin:"cirmos"},
-    {id:2,nev:"Lune", faj:"macska", ar:"500", szin:"cirmos"},
-    {id:3,nev:"Macsa", faj:"macska", ar:"2000", szin:"cirmos"}
+    {id:"ID",nev:"Név", faj:"Faj", ar:"Ár", szin:"Szín", muveletek:"Műveletek"},
+    {id:1,nev:"Micike", faj:"macska", ar:"1000", szin:"cirmos", muveletek:"Szerkesztés/Törlés"},
+    {id:2,nev:"Lune", faj:"macska", ar:"500", szin:"cirmos", muveletek:"Szerkesztés/Törlés"},
+    {id:3,nev:"Macsa", faj:"macska", ar:"2000", szin:"cirmos", muveletek:"Szerkesztés/Törlés"}
 ]
-const oszlopok=["nev", "faj", "ar","szin"]
+const oszlopok=["nev", "faj", "ar","szin", "muveletek"]
 
 function $(mit){
     vissza=document.querySelectorAll(mit)
@@ -21,8 +22,12 @@ function rendel(){
 
             oszlopok.forEach(
                 function(mezoNev){
+                    // head=document.createElement('div')
+                    // head.className="Headline"
+                    // head.innerHTML="Szia!"
                     oszlop=document.createElement('div')
                     oszlop.className="col"
+                    // oszlop.appendChild(head)
                     oszlop.innerHTML=allat[mezoNev]
                     sor.appendChild(oszlop)  
                 }
